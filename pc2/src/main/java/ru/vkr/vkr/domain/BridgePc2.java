5d42a86362e5974d6e3dd0e7e2bd492908774682
Kamo98 <avanesjan.k@gmail.com>
2020-06-21 12:01:38 +0700

    После авторизации теперь происходит очистка списка слушателей, которые обнуляют contest

--- pc2/src/main/java/ru/vkr/vkr/domain/BridgePc2.java
+++ pc2/src/main/java/ru/vkr/vkr/domain/BridgePc2.java
@@ -5,6 +5,9 @@ import edu.csus.ecs.pc2.api.IProblem;
 import edu.csus.ecs.pc2.api.ServerConnection;
 import edu.csus.ecs.pc2.api.exceptions.LoginFailureException;
 import edu.csus.ecs.pc2.api.exceptions.NotLoggedInException;
+import edu.csus.ecs.pc2.api.implementation.ConnectionEventListenerList;
+import edu.csus.ecs.pc2.api.implementation.Contest;
+import edu.csus.ecs.pc2.api.listener.IConnectionEventListener;
 import edu.csus.ecs.pc2.core.InternalController;
 import edu.csus.ecs.pc2.core.model.InternalContest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ import java.io.ObjectInputStream;
 import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Vector;
 
 public class BridgePc2 {
     private static Map<String, ServerConnection> serverConnections = new TreeMap<>();
@@ -37,6 +41,7 @@ public class BridgePc2 {
             internalContests.put(nameCurUser, initInternalContest(serverConnection));
             runStatisticListeners.put(nameCurUser, initContest(contest));
 
+
             for (IProblem problem : contest.getProblems()) {
                 System.out.println(problem.getShortName() + ")  " + problem.getName() + " " + problem.getValidatorFileName());
             }
@@ -46,6 +51,9 @@ public class BridgePc2 {
             }
 
             serverConnections.put(nameCurUser, serverConnection);
+
+            cleanConnectionEventListenerList((Contest) contest);
+
         } catch (LoginFailureException e) {
             System.out.println("Could not login because " + e.getMessage());
         }
@@ -95,6 +103,27 @@ public class BridgePc2 {
             contest.addRunListener(runStatisticListener);
         }
         return runStatisticListener;
+    }
+
+    //Очищает список слушателей ConnectionEventListener, чтобы не обнулялся contest в ServerConnection
+    private static void cleanConnectionEventListenerList (Contest contest) {
+        try {
+            Class classController = contest.getClass();
+            Field fieldController = classController.getDeclaredField("connectionEventListenerList");
+            fieldController.setAccessible(true);
+            Object objectController = fieldController.get(contest);
+            ConnectionEventListenerList listenerList = (ConnectionEventListenerList) objectController;
+
+            Class classController2 = listenerList.getClass();
+            Field fieldController2 = classController2.getDeclaredField("listenerList");
+            fieldController2.setAccessible(true);
+            Object objectController2 = fieldController2.get(listenerList);
+            Vector<IConnectionEventListener> listenerVector = (Vector<IConnectionEventListener>) objectController2;
+            listenerVector.removeAllElements();
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static ServerConnection getServerConnection() {
